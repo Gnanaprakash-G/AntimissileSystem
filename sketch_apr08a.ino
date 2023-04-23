8a3833ed5821268f8f27cfd0ae53e0a7bb587b06
Gnanaprakash G <128076382+Gnanaprakash-G@users.noreply.github.com>
2023-04-23 21:22:05 +0530

    Update sketch_apr08a.ino

--- sketch_apr08a.ino
+++ sketch_apr08a.ino
@@ -1,175 +1,68 @@
-//ARDUINO OBSTACLE AVOIDING CAR//
-// Before uploading the code you have to install the necessary library//
-//AFMotor Library https://learn.adafruit.com/adafruit-motor-shield/library-install //
-//NewPing Library https://github.com/livetronic/Arduino-NewPing// 
-//Servo Library https://github.com/arduino-libraries/Servo.git //
-// To Install the libraries go to sketch >> Include Library >> Add .ZIP File >> Select the Downloaded ZIP files From the Above links //
-
-
-#include <AFMotor.h>  #include <NewPing.h>
-#include <Servo.h> 
-
-#define TRIG_PIN A0 
-#define ECHO_PIN A1 
-#define MAX_DISTANCE 200 
-#define MAX_SPEED 190 // sets speed of DC  motors
-#define MAX_SPEED_OFFSET 20
-
-rewind sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); 
-
-AF_DCMotor motor1(1, MOTOR12_1KHZ); 
-AF_DCMotor motor2(2, MOTOR12_1KHZ);
-AF_DCMotor motor3(3, MOTOR34_1KHZ);
-AF_DCMotor motor4(4, MOTOR34_1KHZ);
-Servo myservo;   
-
-boolean goesForward=false;
-int distance = 100;
-int speedSet = 0;
-
+// Includes the Servo library
+#include <Servo.h>. 
+// Defines Tirg and Echo pins of the Ultrasonic Sensor
+const int trigPin = 10;
+const int echoPin = 11;
+// Variables for the duration and the distance
+long duration;
+int distance;
+Servo myServo1; // Creates a servo object for controlling the servo motor
+Servo myServo2; // Creates a servo object for controlling the servo motor
 void setup() {
-
-  myservo.attach(10);  
-  myservo.write(115); 
-  delay(2000);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600);
+  myServo1.attach(12); // Defines on which pin is the servo motor attached
+  myServo2.attach(13); // Defines on which pin is the servo motor attached
 }
-
 void loop() {
- int distanceR = 0;
- int distanceL =  0;
- delay(40);
- 
- if(distance<=15)
- {
-  moveStop();
-  delay(100);
-  moveBackward();
-  delay(300);
-  moveStop();
-  delay(200);
-  distanceR = lookRight();
-  delay(200);
-  distanceL = lookLeft();
-  delay(200);
-
-  if(distanceR>=distanceL)
+  // rotates the servo motor from 15 to 165 degrees
+  for(int i=15;i<=165;i++){  
+  myServo1.write(i);
+  delay(30);
+  distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
+  if (distance<35)
   {
-    turnRight();
-    moveStop();
-  }else
-  {
-    turnLeft();
-    moveStop();
+    trig();
   }
- }else
- {
-  moveForward();
- }
- distance = readPing();
-}
-
-int lookRight()
-{
-    myservo.write(50); 
-    delay(500);
-    int distance = readPing();
-    delay(100);
-    myservo.write(115); 
-    return distance;
-}
-
-int lookLeft()
-{
-    myservo.write(170); 
-    delay(500);
-    int distance = readPing();
-    delay(100);
-    myservo.write(115); 
-    return distance;
-    delay(100);
-}
-
-int readPing() { 
-  delay(70);
-  int cm = sonar.ping_cm();
-  if(cm==0)
-  {
-    cm = 250;
+  Serial.print(i); // Sends the current degree into the Serial Port
+  Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  Serial.print(distance); // Sends the distance value into the Serial Port
+  Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   }
-  return cm;
+  // Repeats the previous lines from 165 to 15 degrees
+  for(int i=165;i>15;i--){  
+  myServo1.write(i);
+  delay(30);
+  distance = calculateDistance();
+  if (distance<35)
+  {
+    trig();
+  }
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+  }
 }
-
-void moveStop() {
-  motor1.run(RELEASE); 
-  motor2.run(RELEASE);
-  motor3.run(RELEASE);
-  motor4.run(RELEASE);
-  } 
+void trig()
+{
+  for(int i=15;i<=100;i++){  
+  myServo2.write(i);
+  delay(10);
+  }
+  // exit(0);
+}
+// Function for calculating the distance measured by the Ultrasonic sensor
+int calculateDistance(){ 
   
-void moveForward() {
-
- if(!goesForward)
-  {
-    goesForward=true;
-    motor1.run(FORWARD);      
-    motor2.run(FORWARD);
-    motor3.run(FORWARD); 
-    motor4.run(FORWARD);     
-   for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
-   {
-    motor1.setSpeed(speedSet);
-    motor2.setSpeed(speedSet);
-    motor3.setSpeed(speedSet);
-    motor4.setSpeed(speedSet);
-    delay(5);
-   }
-  }
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  distance= duration*0.034/2;
+  return distance;
 }
-
-void moveBackward() {
-    goesForward=false;
-    motor1.run(BACKWARD);      
-    motor2.run(BACKWARD);
-    motor3.run(BACKWARD);
-    motor4.run(BACKWARD);  
-  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
-  {
-    motor1.setSpeed(speedSet);
-    motor2.setSpeed(speedSet);
-    motor3.setSpeed(speedSet);
-    motor4.setSpeed(speedSet);
-    delay(5);
-  }
-}  
-
-void turnRight() {
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(BACKWARD);
-  motor4.run(BACKWARD);     
-  delay(500);
-  motor1.run(FORWARD);      
-  motor2.run(FORWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);      
-} 
- 
-void turnLeft() {
-  motor1.run(BACKWARD);     
-  motor2.run(BACKWARD);  
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);   
-  delay(500);
-  motor1.run(FORWARD);     
-  motor2.run(FORWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
-}  
